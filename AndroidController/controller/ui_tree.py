717fcca85db0c611301a715a5497253c4d0ea1f3
Ujjwal Kumar <125237027+uk2023@users.noreply.github.com>
2026-09-09 07:59:19 +0530

    AndroidController: add UI tree interface

---
+++ AndroidController/controller/ui_tree.py
@@ -0,0 +1,11 @@
+"""Accessibility/UI-tree parsing placeholder."""
+
+
+def get_ui_tree():
+    """Return a normalized accessibility tree. Implementation will be added later."""
+    raise NotImplementedError
+
+
+def find_text(text: str):
+    """Find UI nodes containing text."""
+    raise NotImplementedError
